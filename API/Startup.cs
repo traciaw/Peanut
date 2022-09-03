545cc44f2782fbdbadb39be3fb705ec7d4f7ebe3
[traciaw] <traciawee@gmail.com>
2022-09-04 02:42:16 +0800

    End of s3

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,8 +24,6 @@ namespace API
             _config = config;
         }
 
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -38,6 +36,14 @@ namespace API
             services.AddDbContext<DataContext>(opt=>
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy=>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+                
             });
         }
 
@@ -54,6 +60,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
